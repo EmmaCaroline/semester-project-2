2186cabb5a6b391241a52011de7471f16984fc42
EmmaCaroline <emma-caroline@hotmail.com>
2024-11-28 04:00:19 +0100

    Added dropdowns in the header, and fixed mistake made when adding script tag

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // main js file
+
+//dropdown-menu:
+const dropdownBtn = document.querySelector("#dropdown-menu-btn");
+const dropdownMenu = document.querySelector("#dropdown-menu");
+
+dropdownBtn.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("hidden");
+});
+
+//dropdown-profile:
+const dropdownProfileBtn = document.querySelector("#dropdown-profile-btn");
+const dropdownProfile = document.querySelector("#dropdown-profile");
+
+dropdownProfileBtn.addEventListener("click", () => {
+  dropdownProfile.classList.toggle("hidden");
+});
