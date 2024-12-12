86efe2fc3c0419c1b03457cbd42b3512d83de339
EmmaCaroline <emma-caroline@hotmail.com>
2024-12-12 19:59:04 +0100

    Added event listener for scrolling to view when pressing enter in searchbar

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -15,3 +15,13 @@ onReadAllListings();
 
 initializePagination();
 fetchAndRenderListings();
+
+const searchInput = document.getElementById("search-input");
+
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Prevent default form submission
+    const scrollTarget = document.getElementById("scroll-to-listings");
+    scrollTarget.scrollIntoView({ behavior: "smooth" });
+  }
+});
